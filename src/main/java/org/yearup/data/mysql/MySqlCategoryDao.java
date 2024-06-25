b166e9e86cdac6948a89848b49db24781be73614
gangabiswa <Gbiswa@pgh.year.org>
2024-06-25 13:48:44 -0400

    worked on dao update

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -88,9 +88,19 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
     }
 
     @Override
-    public void update(int categoryId, Category category)
-    {
-        // update category
+    public void update(int categoryId, Category category){
+        String sql = "UPDATE Categories SET name = ?, description = ? WHERE category_id = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.setInt(3, categoryId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
