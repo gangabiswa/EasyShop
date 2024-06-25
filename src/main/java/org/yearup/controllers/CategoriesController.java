47f0acf7a1b534d811777c244462eba5140e3029
gangabiswa <Gbiswa@pgh.year.org>
2024-06-25 13:45:07 -0400

    worked on get product by id

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -41,12 +41,11 @@ public class CategoriesController
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
-        // get a list of product by categoryId
-        return null;
+    @RequestMapping(path = "/{categoryId}/products", method = RequestMethod.GET)
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        return productDao.listByCategoryId(categoryId);
     }
+
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
